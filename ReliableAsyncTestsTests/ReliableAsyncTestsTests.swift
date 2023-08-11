3f82ff04ce1f2f570ae2553b99bde9f4e5c03b3f
Tobi Omotayo <tobitech@ymail.com>
2023-08-11 15:17:10 +0100

    no only is the order of tasks not deterministic but also the scheduling and interleaving of tasks is also non-deterministic

--- ReliableAsyncTestsTests/ReliableAsyncTestsTests.swift
+++ ReliableAsyncTestsTests/ReliableAsyncTestsTests.swift
@@ -48,4 +48,21 @@ final class ReliableAsyncTestsTests: XCTestCase {
 		}
 		XCTAssertEqual(values, Array(1...100))
 	}
+	
+	func testYieldScheduling() async {
+		let count = 10
+		let values = LockIsolated<[Int]>([])
+		let tasks = (0...count).map { n in
+			Task {
+				values.withValue { $0.append(n * 2) }
+				await Task.yield()
+				values.withValue { $0.append(n * 2 + 1) }
+			}
+		}
+		
+		for task in tasks { await task.value }
+		// assuming yield get called after the first task to add even numbers before odd number are now added
+		// we expect to have an array of all the even numbers first and then all the odd numbers second
+		XCTAssertEqual(values.value, [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 1, 3, 5, 7, 9, 11, 13, 17, 19, 21])
+	}
 }
