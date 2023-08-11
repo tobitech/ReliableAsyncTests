cf1496e52c79dbb9ed12e85f8b867eb374be12b7
Tobi Omotayo <tobitech@ymail.com>
2023-08-11 16:38:24 +0100

    now let's try to control the dependency to get a handle of when the task finishes
    using an async stream.
    
    we get failures if we try to run the test 1000 times.

--- ReliableAsyncTestsTests/NumberFactModelTests.swift
+++ ReliableAsyncTestsTests/NumberFactModelTests.swift
@@ -30,8 +30,20 @@ final class NumberFactModelTests: XCTestCase {
 	}
 	
 	func testFactClearsOut() async {
+		// var factContinuation: AsyncStream<String>.Continuation!
+		// let factStream = AsyncStream<String> { continuation in factContinuation = continuation }
+		
+		// we can relace the two lines above with some helpers in the Dependencies package:
+		// this will be provided out of the box in Swift 5.9 rather than depending on the package.
+		let fact = AsyncStream.makeStream(of: String.self)
+		
 		let model = withDependencies {
-			$0.numberFact.fact = { "\($0) is a good number." }
+			// $0.numberFact.fact = { "\($0) is a good number." }
+			// ignore the number that is handed to us and now defer all that to the async stream
+			$0.numberFact.fact = { _ in
+				// as soon as the stream emits something that is what will be returned from the fact.
+				await fact.stream.first(where: { _ in true })!
+			}
 		} operation: {
 			NumberFactModel()
 		}
@@ -40,7 +52,8 @@ final class NumberFactModelTests: XCTestCase {
 
 		let task = Task { await model.getFactButtonTapped() }
 		await Task.yield()
-		XCTAssertEqual(model.fact, nil)
+		XCTAssertEqual(model.fact, nil) // ❌ testFactClearsOut(): XCTAssertEqual failed: ("Optional("An old fact about 0")") is not equal to ("nil")
+		fact.continuation.yield("0 is a good number.")
 		await task.value
 		XCTAssertEqual(model.fact, "0 is a good number.")
 	}
