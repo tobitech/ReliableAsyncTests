525afdfc0b87a29706229f89018a596f1eb798be
Tobi Omotayo <tobitech@ymail.com>
2023-08-11 13:02:27 +0100

    Initial Commit

---
+++ ReliableAsyncTests/ReliableAsyncTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReliableAsyncTestsApp.swift
+//  ReliableAsyncTests
+//
+//  Created by Oluwatobi Omotayo on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReliableAsyncTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
